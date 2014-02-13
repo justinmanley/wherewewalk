5a633892b0edbda7f144eaee31edcff0a1be60de
Justin Manley <manleyjster@gmail.com>
2014-02-13 15:26:06 -0600

    Initialize production branch to contain files adapted for release on uchicago servers.

--- index_template.php
+++ index_template.php
@@ -4,13 +4,10 @@
 	<head>
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 		<link rel="stylesheet" type="text/css" href="../css/style.css">
-		<link href='http://fonts.googleapis.com/css?family=Andada' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=La+Belle+Aurore' rel='stylesheet' type='text/css'>		<script type="text/javascript"
+		<script type="text/javascript"
 			src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=drawing,geometry&sensor=false">
 		</script>
-		<script type="text/javascript"
-			src="http://google-maps-utility-library-v3.googlecode.com/svn/tags/infobox/1.1.9/src/infobox.js">
-		</script>
+		<script type="text/javascript" src="../resources/infobox.js"></script>
 		<script type="text/javascript" src="../mapUtilities.js"></script>
 		<script type="text/javascript" src="../colors/rainbowvis.js"></script>		
 		<script type="text/javascript" src="index.js"></script>
