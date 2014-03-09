b57b76811fef10c41f2e9340b9b869c398c4b180
Justin Manley <manleyjster@gmail.com>
2014-03-09 18:46:55 -0500

    Add <body> tag to /end/index.php.

--- pages/end/index.php
+++ pages/end/index.php
@@ -1,13 +1,14 @@
 <html class="thankyou">
-	<head>
-		<meta charset="UTF-8"/>
-		<link href='http://fonts.googleapis.com/css?family=Andada' rel='stylesheet' type='text/css'/>
-		<link rel="stylesheet" type="text/css" href="../../dowsing-js/css/style.css"/>
-		<link rel="stylesheet" type="text/css" href="../../css/style.css"/>
-		<script>
-			setTimeout("location.href = 'http://facilities.uchicago.edu/about/mission/';", 5000)
-		</script>
-	</head>
+<head>
+	<meta charset="UTF-8"/>
+	<link href='http://fonts.googleapis.com/css?family=Andada' rel='stylesheet' type='text/css'/>
+	<link rel="stylesheet" type="text/css" href="../../dowsing-js/css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="../../css/style.css"/>
+	<script>
+		setTimeout("location.href = 'http://facilities.uchicago.edu/about/mission/';", 5000)
+	</script>
+</head>
+<body>
 <?php 
 
 function successful_database_save() {
@@ -21,4 +22,5 @@ require_once('../../dowsing-js/save.php');
 <div id="github-link">
 	Think this survey is cool?   Check out the code on <a href="https://github.com/manleyjster/dowsing-js">Github</a>
 </div>
+</body>
 </html><!-- .thankyou -->
