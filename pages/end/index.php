69aca14b2d87974f85c7cb14e0bbbb977f158271
Justin Manley <manleyjster@gmail.com>
2014-05-20 18:55:09 -0500

    Submit survey data via an asynchronous POST submission to save.php from end/index.js.

--- pages/end/index.php
+++ pages/end/index.php
@@ -4,23 +4,18 @@
 	<link href='http://fonts.googleapis.com/css?family=Andada' rel='stylesheet' type='text/css'/>
 	<link rel="stylesheet" type="text/css" href="../../spatialsurvey/css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="../../css/style.css"/>
+	<script type="text/javascript"
+		src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=drawing,geometry&sensor=false">
+	</script>	
+	<script type="text/javascript" src="index.js"></script>	
 	<script>
 		setTimeout("location.href = 'http://facilities.uchicago.edu/about/mission/';", 5000)
 	</script>
 </head>
 <body>
-<?php 
-
-function successful_database_save() {
-	echo '<div id="thankyou">Thank you for your input.<br />Your response will help us improve campus.</div>\n';		
-	return;
-}
-
-require_once('../../spatialsurvey/save.php'); 
-
-?>
-<div id="github-link">
-	Think this survey is cool?   Check out the <a href="../../spatialsurvey/docs/spatialsurvey/0.1.0">documentation</a> and the code on <a href="https://github.com/manleyjster/spatialsurvey">Github</a>
-</div>
+	<div id="thankyou">Thank you for your input.<br />Your response will help us improve campus.</div>
+	<div id="github-link">
+		Think this survey is cool?   Check out the <a href="../../spatialsurvey/docs/spatialsurvey/0.1.0">documentation</a> and the code on <a href="https://github.com/manleyjster/spatialsurvey">Github</a>
+	</div>
 </body>
 </html><!-- .thankyou -->
